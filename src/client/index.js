dca9f4d534770f2ef465330a841846fc7c4a6092
Caroline <carolineverpoorten@gmail.com>
2020-10-13 09:14:01 +0200

    prepare for pull

--- src/client/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import MapOpen from "./MapOpen";
-import Overlay from "./overlay";
-
-ReactDOM.render(<Overlay />, document.querySelector("#app"));
-ReactDOM.render(<MapOpen />, document.querySelector("#root"));
